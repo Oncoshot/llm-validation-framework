55399ac5a29333c35cb7f834c14f4befb5c08bb4
Andrey Gonostarev <andrey@oncoshot.com>
2026-02-19 23:56:36 +0100

    fix: added ci-cd

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
+from importlib.metadata import version, PackageNotFoundError
 
+try:
+    __version__ = version("oncoshot-llm-validation-framework")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
