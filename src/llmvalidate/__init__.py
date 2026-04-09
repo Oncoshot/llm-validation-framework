3a28b7d08bf86b1b09efc4f3d83a328daabdb5ad
tidiane <tidiane@oncoshot.com>
2026-04-09 17:30:00 +0800

    ONC-11999 change init.py to allow "import llmvalidate as v"

--- src/llmvalidate/__init__.py
+++ src/llmvalidate/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "0.0.0"
+__version__ = "0.4.2"
+
+from .validation import validate, bootstrap_CI
+from .structured import StructuredResult, StructuredGroup, StructuredField
+
+__all__ = [
+    "validate", 
+    "bootstrap_CI", 
+    "StructuredResult", 
+    "StructuredGroup", 
+    "StructuredField"
+]
